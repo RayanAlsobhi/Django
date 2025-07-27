f257ab436579bbb10f0130b19d9ad95bae0dbb87
Rayan Alsobhi <ryn3545@yahoo.com>
2025-07-27 06:51:07 +0300

    added all the new files

--- playground_app/views.py
+++ playground_app/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist 
+from django.db.models import Q,F
+from store.models import Product,OrderItem
 # i try to statr debuging but not work and i just import "debugpy" and it work 
 # import debugpy
 # debugpy.listen(("localhost", 5678))
 # debugpy.wait_for_client()
 
-def calculate():
-    x  = 1
-    y = 2 
-    return x
+
 def say_hello(request):
-    x = calculate()
-    y = 2
-    z = x + y
-    return render(request , 'hello.html',{'name':'Rayan'})
+    
+    queryset = OrderItem.objects.values('product_id').distinct()
+    
+    
+    return render(request , 'hello.html',{'name':'Rayan','queryset':list(queryset)})
+
+
